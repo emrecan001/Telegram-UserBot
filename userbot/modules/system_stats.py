88636862c24e888c638288e97fe2658635a2918a
EmreCan Ayas <34011883+romman001@users.noreply.github.com>
2020-02-16 18:48:50 +0300

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -138,8 +138,7 @@ async def amireallyalive(alive):
                      "Your bot is running \n\n"
                      f"Telethon version: {version.__version__} \n"
                      f"Python: {python_version()} \n"
-                     f"User: {DEFAULTUSER} \n"
-                     f"Database Status: {db}"
+                     f"User: {DEFAULTUSER} \n"          
                      "`")
 
 
